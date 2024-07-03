a1a32d6be5e4406ff29fd7dad24a635861c2c78b
Reuben <136352370+ReubenMatrix@users.noreply.github.com>
2024-07-03 23:28:53 +0530

    Complete

--- api/routes/Notes.js
+++ api/routes/Notes.js
@@ -49,17 +49,7 @@ router.get('/getnotes',  authMiddleware, async (req, res) => {
 });
 
 
-router.get('/note/:id',  authMiddleware, async (req, res) => {
-  try {
-    const note = await Note.findById(req.params.id);
-    if (!note) {
-      return res.status(404).json({ error: 'Note not found' });
-    }
-    res.json(note);
-  } catch (err) {
-    res.status(500).json({ error: err.message });
-  }
-});
+
 
 
 router.put('/note/:id', authMiddleware, async (req, res) => {
